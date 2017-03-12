195bc5ac238703fccbca678acb4b06e1ebd435a7
cdnutter <cdnutter@gmail.com>
2017-03-12 14:05:33 -0700

    Fixed gearsubsystem

---
+++ src/org/usfirst/frc/team4141/robot/subsystems/GearSubsystem.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team4141.robot.subsystems;
+
+import org.usfirst.frc.team4141.MDRobotBase.MDRobotBase;
+import org.usfirst.frc.team4141.MDRobotBase.MDSubsystem;
+import org.usfirst.frc.team4141.MDRobotBase.config.ConfigSetting;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class GearSubsystem extends MDSubsystem {
+	
+	private double liftSpeed=0.2;
+	
+	private SpeedController ropeController;
+	
+	public static String motorName="ropeMotor";
+
+	public MDSubsystem configure(){
+		super.configure();
+
+		if(getMotors()==null 
+				|| !getMotors().containsKey(motorName))
+			throw new IllegalArgumentException("Invalid motor configuration for rope system.");
+		ropeController = (SpeedController)(getMotors().get(motorName));
+		//setCore(true);
+		return this;
+	}
+	
+	public GearSubsystem(MDRobotBase robot, String name) {
+		super(robot, name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void move(){
+		//positive speed=wind
+		//negative speed=unwind
+		ropeController.set(liftSpeed);
+	}
+		
+	public void stop(){
+		ropeController.stopMotor();
+		
+	}
+	
+	@Override
+	protected void setUp() {
+		
+		if(getConfigSettings().containsKey("liftSpeed")) liftSpeed = getConfigSettings().get("liftSpeed").getDouble();
+		
+	}
+
+	@Override
+	public void settingChangeListener(ConfigSetting changedSetting) {
+		
+		if(changedSetting.getName().equals("liftSpeed")) liftSpeed = changedSetting.getDouble();
+
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
